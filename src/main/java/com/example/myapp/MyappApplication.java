cf287a57085f5b2b43a3e36e11e1f6dd14e44fe4
Fabiano Goes <fabianogoes@gmail.com>
2017-03-26 00:22:51 -0300

    setup travis openshift

--- src/main/java/com/example/myapp/MyappApplication.java
+++ src/main/java/com/example/myapp/MyappApplication.java
@@ -2,11 +2,12 @@ package com.example.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 @SpringBootApplication
 public class MyappApplication {
 
@@ -16,7 +17,12 @@ public class MyappApplication {
 	
 	@RequestMapping
 	public String welcome(){
-		return "Welcome to Myapp on Openshift!!!";
+		return "Welcome to API";
+	}
+
+	@RequestMapping("/{name}")
+	public String welcome(@PathVariable("name") String name){
+		return "Welcome [ "+name+" ]...";
 	}
 	
 }
